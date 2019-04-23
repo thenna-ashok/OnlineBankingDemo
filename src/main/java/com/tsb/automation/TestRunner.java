ec08ee3a635048feabc0264da40f4038100ad2d0
thenna <thenna.mail@gmail.com>
2019-04-24 06:46:04 +1200

    Modified TestRunner cucumber options arguments

--- src/main/java/com/tsb/automation/TestRunner.java
+++ src/main/java/com/tsb/automation/TestRunner.java
@@ -6,13 +6,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"pretty", "json:target/json/output.json" },
-        plugin = {"html:target/cucumber-html-report", "junit:target/cucumber-junit.xml"},
-        features = {"src/main/java/com/tsb/automation/features/"}
-        //glue = {"stepdefs"}
-        // dryRun = false,
-        // strict = true,
-       // monochrome = true
+@CucumberOptions(
+        features = "src/main/java/com/tsb/automation/features",
+        glue = "stepdefs",
+        plugin = {"html:target/cucumber-html-report"}
 )
 
 public class TestRunner {
